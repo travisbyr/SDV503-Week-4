33ea147fd1c7fd0fabe6be0f86a575da49cfe951
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2020-03-16 12:40:21 +1300

    update

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var colour = "red";
 
 if (colour == "red") {
+  // If colour is red, then print below
   console.log("The colour is red");
+} else if (colour == "green") {
+  // Used to not conflict with else.
+  // If colour is green, then print below
+  console.log("The colour is green");
 } else {
+  // If colour does not apply to above, print below
   console.log("This is another colour");
 }
+
+
